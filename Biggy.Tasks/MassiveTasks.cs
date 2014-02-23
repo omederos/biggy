b976a972fbfb4ac7d769db29d9bab924abf74b43
xivsolutions <xivsolutions@gmail.com>
2014-02-23 12:13:04 -0600

    Updated Namespace so Massive is included for testing

--- Biggy.Tasks/MassiveTasks.cs
+++ Biggy.Tasks/MassiveTasks.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Massive;
+using Biggy.Massive;
 using System.Diagnostics;
 
 namespace Biggy.Tasks
