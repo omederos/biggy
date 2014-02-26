74e4bec251b82021ca9b3d99c882e4340cd003fc
Brendan Forster <brendan@github.com>
2014-02-26 18:50:59 +1100

    verbosity == quiet

--- build.ps1
+++ build.ps1
@@ -5,7 +5,7 @@ $currentDir = Split-Path $MyInvocation.MyCommand.Path
 $MsBuildDir = (Get-ItemProperty -ea:SilentlyContinue ("HKLM:\Software\Microsoft\MSBuild\ToolsVersions\12.0")).MSBuildToolsPath
 $msbuild = Join-Path $MsBuildDir msbuild.exe
 
-. $msbuild "$currentDir\Biggy.sln" /target:Rebuild /property:Configuration=Release
+. $msbuild "$currentDir\Biggy.sln" /target:Rebuild /property:Configuration=Release /v:q
 
 $nuget = Join-Path $currentDir tools\nuget.exe
 
