627e57680031a30141fda1b41f1efd7ca4abe8db
Brendan Forster <brendan@github.com>
2014-02-26 18:40:56 +1100

    a simple build script added

---
+++ build.ps1
@@ -0,0 +1,8 @@
+# assumes you have VS2013 or the standalone MSBuild tools available
+
+$currentDir = Split-Path $MyInvocation.MyCommand.Path
+
+$MsBuildDir = (Get-ItemProperty -ea:SilentlyContinue ("HKLM:\Software\Microsoft\MSBuild\ToolsVersions\12.0")).MSBuildToolsPath
+$msbuild = Join-Path $MsBuildDir msbuild.exe
+
+. $msbuild "$currentDir\Biggy.sln" /target:Rebuild /property:Configuration=Release
