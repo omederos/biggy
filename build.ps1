f0dd207aa1bc4bd522ce12cd6760ce30c1fa6b27
Iain Partington <ipartington@gmail.com>
2014-02-27 22:29:43 +0000

    Added Git Restore to build script

--- build.ps1
+++ build.ps1
@@ -1,9 +1,13 @@
 # assumes you have VS2013 or the standalone MSBuild tools available
 
+
+
 $currentDir = Split-Path $MyInvocation.MyCommand.Path
 
 $MsBuildDir = (Get-ItemProperty -ea:SilentlyContinue ("HKLM:\Software\Microsoft\MSBuild\ToolsVersions\12.0")).MSBuildToolsPath
 $msbuild = Join-Path $MsBuildDir msbuild.exe
+
+.\tools\nuget.exe restore
 
 . $msbuild "$currentDir\Biggy.sln" /target:Rebuild /property:Configuration=Release /v:q
 
